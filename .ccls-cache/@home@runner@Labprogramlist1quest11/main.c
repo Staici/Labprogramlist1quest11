87220234fefacba7592560a6c368725effa76a48
Steicie Aleixo Sousa <51095043+Staici@users.noreply.github.com>
2024-04-24 12:43:37 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Labprogramlist1quest11/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+  int vlreal;
+  int real;
+   printf("Digite um valor em reais para ser convertido em dólar");
+     scanf("%d",&real);
+  vlreal= real * 5;
+  printf("O valor em real é: %d", vlreal);
+  
+  
+  return 0;
+}
